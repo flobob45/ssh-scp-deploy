89c3ee6e8f2dc03b729638f12d3d7878b4c8cc3c
Florian BOURGOIN <florian.bourgoin@worldline.com>
2022-09-08 00:01:36 +0200

    debug

--- with_key.sh
+++ with_key.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -x
 echo "🔑 Adding ssh key..." &&
 eval $(ssh-agent -s) &&
 ssh-add <(echo "${INPUT_KEY}") && 
@@ -9,7 +9,7 @@ PRE_UPLOAD=${INPUT_PRE_UPLOAD}
 if [ ! -z "$PRE_UPLOAD" ]; then
     { 
         echo "👌 Executing pre-upload script..." &&
-        ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
+        ssh -v ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
         echo "✅ Executed pre-upload script"
     } || { 
         echo "😢 Something went wrong during pre-upload script" && exit 1
